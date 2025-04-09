3552685690436dd09d69019995abcb8c279d53c9
kamilaolejarz <kamila.olejarz2301@gmail.com>
2025-04-09 14:50:08 +0200

    wip: added remove user function

--- utils/controller.py
+++ utils/controller.py
@@ -10,3 +10,11 @@ def add_user(users_date: list) -> None:
     new_location: str = input('Podaj skąd pochodzi nowy znajomy: ')
     new_posts: int = int(input('Podaj liczbę postów: '))
     users_date.append({'name': new_name, 'Location': new_location, 'Posts': new_posts}, )
+
+
+
+def remove_user(users_date: list) -> None:
+    user_name: str = input('Kogo chcesz usunąć: ')
+    for user in users_date:
+        if user['name'] == user_name:
+            users_date.remove(user)
